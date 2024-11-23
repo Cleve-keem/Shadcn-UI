b4d97418d254b7b648a887bb8c2fa92d1ee36f39
hakeem <belloharyourmidey@gmail.com>
2024-11-23 15:42:15 +0100

    uploading

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import './App.css'
 import { Terminal } from "lucide-react";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="searchContainer flex items-center justify-center relative">
-        <Input type="file" disabled placeholder="search..." className="border-black border-2"/>
-        <Button variant="default" className="absolute right-0">Search</Button>     
-      </div>
-    
     <div>
       <Alert >
           <Terminal className="h-4 w-4" />
@@ -22,8 +25,29 @@ function App() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sed nihil soluta eos neque ducimus ea sint.  
           </AlertDescription>  
         </Alert> 
+
+        <AlertDialog>
+          <div className="searchContainer flex items-center justify-center relative">
+            <Input type="text" placeholder="Search..." className="border-black border-2"/>
+            <AlertDialogTrigger>
+              <Button variant="default" className="absolute right-0 top-0">Search</Button>
+            </AlertDialogTrigger>
+          </div>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
     </div>
-    </>
   )
 }
 
